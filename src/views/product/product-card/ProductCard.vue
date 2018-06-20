06248210b88fbed23922f2b5d9507ea2eea7cb9f
Guilherme Silva <guisilva.master@gmail.com>
2018-06-20 14:55:15 -0300

    Create ProductCard component

---
+++ src/views/product/product-card/ProductCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="image" :alt="title" />
+      </figure>
+    </div>
+
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title">{{ title }}</p>
+          <p class="subtitle">{{ price }}</p>
+        </div>
+      </div>
+
+      <div class="content">
+        {{ description }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      erquired: false
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    },
+    image: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
