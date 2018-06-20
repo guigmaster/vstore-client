ffcd76889aa95e224a77d0498977c4e5e040a11e
Guilherme Silva <guisilva.master@gmail.com>
2018-06-20 15:43:33 -0300

    Add a mock product list

--- src/views/product/Product.vue
+++ src/views/product/Product.vue
@@ -1,14 +1,27 @@
 <template>
   <section>
     <div class="container">
-      <h1>Welcome to Product</h1>
+      <div class="columns is-multiline is-mobile" style="margin-top: 10px;">
+        <div class="column is-3-desktop is-4-tablet is-10-mobile is-offset-1-mobile"
+          v-for="i in (0, 20)"
+          :key="i">
+          <product-card
+            :title="'produto de número ' + i"
+            description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quia, obcaecati consectetur at corrupti corporis!"
+            :price="'10.65'"
+            quantity="100" />
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-
+import ProductCard from './product-card/ProductCard'
 export default {
-  name: 'Product'
+  name: 'Product',
+  components: {
+    ProductCard
+  }
 }
 </script>
