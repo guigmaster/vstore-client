7099253f4315e3e6e0d22dbbbc38086cabd38cfe
Guilherme Silva <guisilva.master@gmail.com>
2018-06-20 18:54:24 -0300

    Create a children route for product

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,27 @@
 import Product from '@/views/product/Product'
+import ProductList from '@/views/product/product-list/ProductList'
+import ProductForm from '@/views/product/product-form/ProductForm'
 
 export default [
   {
     path: '/',
-    name: 'product',
-    component: Product
+    redirect: { name: 'products.index' }
+  },
+  {
+    name: 'products.index',
+    path: '/products',
+    component: Product,
+    children: [
+      {
+        name: 'products.list',
+        path: '/',
+        component: ProductList
+      },
+      {
+        name: 'products.new',
+        path: 'create',
+        component: ProductForm
+      }
+    ]
   }
 ]
