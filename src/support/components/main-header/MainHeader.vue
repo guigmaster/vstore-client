53626b9b7cb1fd4f38b690f7abe67ed622d5f1ab
Guilherme Silva <guisilva.master@gmail.com>
2018-06-20 11:28:47 -0300

    Add brand to header

--- src/support/components/main-header/MainHeader.vue
+++ src/support/components/main-header/MainHeader.vue
@@ -1,6 +1,10 @@
 <style lang="scss" src="./MainHeader.scss" scoped></style>
 <template>
-  <header class="navbar is-dark"></header>
+  <header class="navbar is-link main-header">
+    <div class="container">
+      <div class="navbar-brand main-header__brand">VStore</div>
+    </div>
+  </header>
 </template>
 
 <script>
